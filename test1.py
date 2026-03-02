eb3c4e4aefa9fbe1343465175795274c01f16db7
swjee <49114050+swjee@users.noreply.github.com>
2026-03-02 21:21:15 +0900

    Use FuncAnimation event loop so GUI figure reliably opens

--- test1.py
+++ test1.py
@@ -1,7 +1,19 @@
 import time
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
+
+# GUI 백엔드가 아닌 경우(예: Agg) 창이 뜨지 않으므로 인터랙티브 백엔드로 전환 시도
+if matplotlib.get_backend().lower().endswith('agg'):
+    for candidate_backend in ('TkAgg', 'Qt5Agg'):
+        try:
+            matplotlib.use(candidate_backend, force=True)
+            break
+        except Exception:
+            pass
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from CoinInvest import DBRead
 
@@ -19,6 +31,9 @@ def play(text):
 
 
 print ('test DBREAD')
+print(f"matplotlib backend: {matplotlib.get_backend()}")
+if matplotlib.get_backend().lower().endswith('agg'):
+    print('WARNING: GUI backend not available. 화면 창이 표시되지 않을 수 있습니다.')
 
 # 1m, 3m, 5m, 10m, 30m, 1h, 6h, 12h, 24h
 
@@ -32,10 +47,16 @@ PB_UP_TH = 0.8
 PB_DW_TH = 0.2
 MFI_UP_TH =70
 MFI_DW_TH =20
-while True:
+
+fig, axes = plt.subplots(4, 1, figsize=(24, 16))
+
+
+def update(_frame=None):
     dbu = DBRead.DBReader()
     df = dbu.read_xrp(coin_type='XRP')
-    fig = plt.figure(figsize=(24, 16) )
+
+    for ax in axes:
+        ax.clear()
 
     df['MA20'] = df['close'].rolling(window=20).mean()
     df['stddev'] = df['close'].rolling(window=20).std()
@@ -79,36 +100,31 @@ while True:
     df['MFI10']= 100-100/( 1+ df['MFR'])
     df=df[19:]
 
-    plt.subplot(4, 1, 1)
-    plt.title('XRP')
-    plt.plot(df.index, df['close'],  color='#0000ff',label='Close')
-    plt.plot(df.index, df['upper'], 'r--', color='r',label='Upper')
-    plt.plot(df.index, df['MA20'], 'k--', color='b',label='MA20')
-    plt.plot(df.index, df['lower'], 'c--', color='g',label='Lower')
+    axes[0].set_title('XRP')
+    axes[0].plot(df.index, df['close'],  color='#0000ff',label='Close')
+    axes[0].plot(df.index, df['upper'], 'r--', color='r',label='Upper')
+    axes[0].plot(df.index, df['MA20'], 'k--', color='b',label='MA20')
+    axes[0].plot(df.index, df['lower'], 'c--', color='g',label='Lower')
 
     for i in range(len(df.close) - 1):
 
         if df.PB.values[i] < 0.1 and df.IIP21.values[i]>0 :
-            plt.plot(df.index.values[i], df.close.values[i], 'k^')
+            axes[0].plot(df.index.values[i], df.close.values[i], 'k^')
             if i > len(df.close) - 3:
-                #ctypes.windll.user32.MessageBoxW(0, "반전 매수타이밍.!", "알림", 1)
                 play("반전 매수 타이밍입니다.")
         elif df.PB.values[i] > 0.9 and df.IIP21.values[i]<0 :
-            plt.plot(df.index.values[i], df.close.values[i], 'yv')
+            axes[0].plot(df.index.values[i], df.close.values[i], 'yv')
             if i > len(df.close) - 3 :
-                #ctypes.windll.user32.MessageBoxW(0, "반전 매도타이밍.!", "알림", 1)
                 play("반전 매도 타이밍입니다.")
 
 
         if  df.PB.values[i] > PB_UP_TH and df.MFI10.values[i] > MFI_UP_TH:
-            plt.plot( df.index.values[i],df.close.values[i],'r^')
+            axes[0].plot( df.index.values[i],df.close.values[i],'r^')
             if i > len(df.close) - 3:
-                #ctypes.windll.user32.MessageBoxW(0, "매수추세!", "알림", 1)
                 play("매수 추세!.")
         elif df.PB.values[i] < PB_DW_TH and df.MFI10.values[i] < MFI_DW_TH :
-            plt.plot( df.index.values[i],df.close.values[i],'bv')
+            axes[0].plot( df.index.values[i],df.close.values[i],'bv')
             if i > len(df.close) - 3:
-                #ctypes.windll.user32.MessageBoxW(0, "매도추세!", "알림", 1)
                 play("매도 추세!.")
 
 
@@ -119,67 +135,48 @@ while True:
             elif df.SQRT_BWCHG.values[i] > 0.15 and df.II.values[i] < 0:
                     play("급락 추세!.")
 
-    plt.legend(loc='upper left')
-    plt.grid(True)
+    axes[0].legend(loc='upper left')
+    axes[0].grid(True)
 
 
-    plt.subplot(4,1,2)
-    plt.title('PB&MFI')
-    plt.plot(df.index, df['PB']*100,  color='b',label='%B * 100')
-    plt.plot(df.index, df['MFI10'], 'g--',label='MFI 10 DAY')
+    axes[1].set_title('PB&MFI')
+    axes[1].plot(df.index, df['PB']*100,  color='b',label='%B * 100')
+    axes[1].plot(df.index, df['MFI10'], 'g--',label='MFI 10 DAY')
 
 
     for i in range(len(df.close) - 1):
         if df.PB.values[i] > PB_UP_TH and df.MFI10.values[i] > MFI_UP_TH:
-            plt.plot(df.index.values[i], 0 , 'r^')
+            axes[1].plot(df.index.values[i], 0 , 'r^')
         elif df.PB.values[i] < PB_DW_TH and df.MFI10.values[i] < MFI_DW_TH:
-            plt.plot(df.index.values[i], 0 , 'bv')
+            axes[1].plot(df.index.values[i], 0 , 'bv')
 
-    plt.legend(loc='upper left')
-    plt.grid(True)
+    axes[1].legend(loc='upper left')
+    axes[1].grid(True)
 
 
 #volume BOL
-    plt.subplot(4, 1, 3)
-    plt.title('VOL_BOL')
-    plt.plot(df.index, df['volume'],  color='k',label='volume')
-    plt.plot(df.index, df['V_upper'], 'r--', color='r',label='V_Upper')
-    plt.plot(df.index, df['V_MA20'], 'k--', color='b',label='V_MA20')
-    plt.plot(df.index, df['V_lower'], 'c--', color='g',label='V_Lower')
+    axes[2].set_title('VOL_BOL')
+    axes[2].plot(df.index, df['volume'],  color='k',label='volume')
+    axes[2].plot(df.index, df['V_upper'], 'r--', color='r',label='V_Upper')
+    axes[2].plot(df.index, df['V_MA20'], 'k--', color='b',label='V_MA20')
+    axes[2].plot(df.index, df['V_lower'], 'c--', color='g',label='V_Lower')
 
-    plt.legend(loc='upper left')
-    plt.grid(True)
+    axes[2].legend(loc='upper left')
+    axes[2].grid(True)
 
-#    #  II%.. plot
-#    plt.subplot(4,1,3)
-#    plt.title('IIP21')
-#    plt.plot(df.index, df['IIP21'],  color='g',label='II% 21day')
-#
-#    for i in range(len(df.close) - 1):
-#        if df.PB.values[i] < 0.1 and df.IIP21.values[i] > 0:
-#            plt.plot(df.index.values[i], 0 , 'r^')
-#        elif df.PB.values[i] > 0.9 and df.IIP21.values[i] < 0:
-#            plt.plot(df.index.values[i], 0 , 'bv')
-#
-
-    plt.legend(loc='upper left')
-    plt.grid(True)
-
-    #  volume.. plot
-    plt.subplot(4,1,4)
-    plt.title('V_BW*BW')
-#    plt.plot(df.index, df['V_PB'],  color='k',label='V_PB')
-#    plt.plot(df.index, df['PB'],  color='b',label='PB')
-    plt.plot(df.index, df['SQRT_BW'],  color='k',label='SQRT BW*BBW')
+    axes[3].set_title('V_BW*BW')
+    axes[3].plot(df.index, df['SQRT_BW'],  color='k',label='SQRT BW*BBW')
     if df.SQRT_BWCHG.values[len(df.SQRT_BWCHG)-1] >=0 :
-        plt.plot(df.index, df['SQRT_BWCHG'],  color='r',label='SQRT BWCHG')
+        axes[3].plot(df.index, df['SQRT_BWCHG'],  color='r',label='SQRT BWCHG')
     else:
-        plt.plot(df.index, df['SQRT_BWCHG'],  color='b',label='SQRT BWCHG')
+        axes[3].plot(df.index, df['SQRT_BWCHG'],  color='b',label='SQRT BWCHG')
+
+    axes[3].legend(loc='upper left')
+    axes[3].grid(True)
+
+    fig.tight_layout()
 
 
-    plt.legend(loc='upper left')
-    plt.grid(True)
-
-
-    plt.pause(10)
-    plt.close()
+ani = FuncAnimation(fig, update, interval=10000, cache_frame_data=False)
+update()
+plt.show()
